a2133d353f3c3419638a113070a485669c9c4195
Youngil Choi <duddlf.choi@samsung.com>
2016-08-17 18:42:47 +0900

    Change board name for STM32F4-discovery to fix broken iotjs-nuttx build

--- cmake/option/option_arm-nuttx.cmake
+++ cmake/option/option_arm-nuttx.cmake
@@ -38,7 +38,7 @@ set(PLATFORM_SRCFILES
 set(PLATFORM_TESTFILES "${TEST_ROOT}/runner_nuttx.c")
 
 if(DEFINED TARGET_BOARD)
-  if(${TARGET_BOARD} STREQUAL "stm32f4")
+  if(${TARGET_BOARD} STREQUAL "stm32f4dis")
     set(FLAGS_COMMON
           ${FLAGS_COMMON}
           "-mcpu=cortex-m4"
