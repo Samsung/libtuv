c336586e1b298b756150d6b9173fda3faa9d9c28
Konrad Lipner <k.lipner@samsung.com>
2017-02-13 12:03:00 +0100

    Update dates
    libtuv-DCO-1.0-Signed-off-by: Konrad Lipner k.lipner@samsung.com

--- cmake/config/config_arm-tizen.cmake
+++ cmake/config/config_arm-tizen.cmake
@@ -1,4 +1,4 @@
-# Copyright 2015-2016 Samsung Electronics Co., Ltd.
+# Copyright 2017 Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
