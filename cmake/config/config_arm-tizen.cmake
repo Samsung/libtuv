b49100a46cae68553e4789f6224fd56d1d0ce7de
Philippe Coval <philippe.coval@osg.samsung.com>
2017-07-26 16:45:57 +0200

    tizen: make compiler overridable for GBS
    There is no arm-linux-gnueabi-gcc in Tizen's sysroot,
    so I assume it is used for cross compilation,
    not using Tizen's build tools.
    
    To build using GBS (git-build-system),
    this variable can be used to use system compiler "gcc" this way:
    
      cmake -DCMAKE_C_COMPILER=gcc .
    
    Observed issue on Tizen:4.0:Unified (armv7l) was:
    
      -- Check for working C compiler: arm-linux-gnueabi-gcc
      CMake Error: your C compiler: "arm-linux-gnueabi-gcc" was not found.
      Please set CMAKE_C_COMPILER to a valid compiler path or name.
    
    libtuv-DCO-1.0-Signed-off-by: Philippe Coval philippe.coval@osg.samsung.com

--- cmake/config/config_arm-tizen.cmake
+++ cmake/config/config_arm-tizen.cmake
@@ -17,4 +17,6 @@ include(CMakeForceCompiler)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-SET(CMAKE_C_COMPILER   arm-linux-gnueabi-gcc)
+if(NOT DEFINED CMAKE_C_COMPILER)
+  set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+endif()
