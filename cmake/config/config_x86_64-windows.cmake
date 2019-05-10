520737fd77e098be4e72beb4334957f242e4fbc7
Laszlo Lango <llango.u-szeged@partner.samsung.com>
2019-05-10 11:34:08 +0200

    Added x86_64 toolchain files for Windows target.
    libtuv-DCO-1.0-Signed-off-by: Laszlo Lango llango.u-szeged@partner.samsung.com

--- cmake/config/config_x86_64-windows.cmake
+++ cmake/config/config_x86_64-windows.cmake
@@ -1,4 +1,4 @@
-# Copyright 2015 Samsung Electronics Co., Ltd.
+# Copyright 2019-present Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# windows common
-include("cmake/option/option_windows_common.cmake")
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x64)
 
-# i686 specific
-
+if(NOT DEFINED CMAKE_C_COMPILER)
+  set(CMAKE_C_COMPILER CL.exe)
+endif()
